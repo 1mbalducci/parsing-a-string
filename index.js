c6dd5b47a73b1ad3eb0dbded27d16ee35d6329a7
Michelle Balducci <93694720+1mbalducci@users.noreply.github.com>
2022-05-23 22:31:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function detectWord(str) {
+	const paragraph = "UcUNFYGaFYFYGtNUH";
+const regex = /[a-z]/g;
+const found = paragraph.match(regex);
+
+console.log(found.join(''));
+}
+detectWord("UcUNFYGaFYFYGtNUH");
